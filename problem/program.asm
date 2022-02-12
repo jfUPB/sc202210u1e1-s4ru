8768f6557b2cc53c5a56e4699f60a02e9e184779
s4ru <83980881+s4ru@users.noreply.github.com>
2022-02-12 10:05:51 -0500

    Challenge 1-2

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,46 @@
-// David Rivera Cordoba
+// David Rivera Cordoba 
 // 000416335
 // david.rivera@upb.edu.co
-(START)
-    @START
-    0;JMP
+
+@24576
+D=M
+@70
+D=D-A
+@14
+D;JEQ
+@24576
+D=M
+@67
+D=D-A
+@28
+D;JEQ
+@0
+0;JMP
+@16
+M=-1
+@16
+M=M+1
+D=M
+@16384
+A=D+A
+M=-1
+@8192
+D=D-A
+@16
+D;JNE
+@0
+0;JMP
+@16
+M=-1
+@16
+M=M+1
+D=M
+@16384
+A=D+A
+M=0
+@8192
+D=D-A
+@30
+D;JNE
+@0
+0;JMP
